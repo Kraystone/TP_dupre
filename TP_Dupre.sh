8380927a104b412b08b8d6102ad7a3d01fefb5d6
Kraystone <leo.catifait@epsi.fr>
2020-11-01 14:20:59 +0100

    menu

--- TP_Dupre.sh
+++ TP_Dupre.sh
@@ -1,27 +1,84 @@
 #!/bin/bash
 IP="iptables"
+MARRON='\033[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+###############################################WEB#########################################
+if [ "$HOSTNAME" = web ]; then
+  printf '%s\n' "Execution du script pour la machine WEB"
+  #La fonction pause, assez utile pour .... mettre en pause
+pause (){
+    echo -e "${GREEN}Appuyer sur une touche ${NC}"
+    read -p "$*"
+}
+while :
+do 
+echo -e "
+${GREEN}---Menu du Script---${NC}
 
-# On vide toutes les règles
-${IP} -F INPUT
-${IP} -F OUTPUT
-${IP} -F FORWARD
-${IP} -x
-${IP}  -A INPUT -p tcp --dport 22 -j ACCEPT
-${IP} -A OUTPUT -p tcp --sport 22 -j ACCEPT
-${IP} -A OUTPUT -p tcp -o ens33 --dport 80 -j ACCEPT
-${IP} -A INPUT -p tcp --sport 80 -i ens33 -j ACCEPT
-${IP} -A OUTPUT -p TCP --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-${IP} -A OUTPUT -p TCP --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-${IP} -A OUTPUT -p UDP --dport 53 -m state --state NEW -j ACCEPT
+${MARRON}1- Secure that stuff.
+2- Parsage des log.
+3- Ban all the lamers.
+4- Quitter le script --->[].${NC}
+"
+read choix
+stty echo
+if [ $choix = 1]; then
+  echo -e "Ecriture des regles" #ALED LE FR marion ?!
+  echo "${IP} -F INPUT
+        ${IP} -F OUTPUT
+        ${IP} -F FORWARD
+        ${IP} -x
+        
+        MARION
 
-# ^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.0)(.*)$ -> utiliser pour récuperer la liste des ip uniquement à partir de "https://ipinfo.io/AS14061" 
-# -> en remplaçant la chaine par uniquement le premier groupe de selection
-${IP} -A INPUT -s 104.131.0.0/16 -j DROP
-${IP} -A INPUT -s 104.236.0.0/16 -j DROP
-${IP} -A INPUT -s 104.248.0.0/16 -j DROP
-${IP} -A INPUT -s 107.170.0.0/16 -j DROP
-${IP} -A INPUT -s 128.199.0.0/16 -j DROP
-${IP} -A INPUT -s 138.68.0.0/16 -j DROP
-${IP} -A INPUT -s 134.122.0.0/16 -j DROP
-${IP} -A INPUT -s 138.197.0.0/16 -j DROP
-${IP} -A INPUT -s 134.209.0.0/16 -j DROP
+        ${IP} -A INPUT -s 104.131.0.0/16 -j DROP
+        ${IP} -A INPUT -s 104.236.0.0/16 -j DROP
+        ${IP} -A INPUT -s 104.248.0.0/16 -j DROP
+        ${IP} -A INPUT -s 107.170.0.0/16 -j DROP
+        ${IP} -A INPUT -s 128.199.0.0/16 -j DROP
+        ${IP} -A INPUT -s 138.68.0.0/16 -j DROP
+        ${IP} -A INPUT -s 134.122.0.0/16 -j DROP
+        ${IP} -A INPUT -s 138.197.0.0/16 -j DROP
+        ${IP} -A INPUT -s 134.209.0.0/16 -j DROP" >  /etc/iptables/iptablesMarion.rules
+  
+fi
+###############################################BDD#########################################
+if [ "$HOSTNAME" = bdd ]; then
+  printf '%s\n' "Execution du script pour la machine BDD"
+pause (){
+    echo -e "${GREEN}Appuyer sur une touche ${NC}"
+    read -p "$*"
+}
+while :
+do 
+echo -e "
+${GREEN}---Menu du Script---${NC}
+
+${MARRON}1- Secure that stuff.
+2- Parsage des log.
+3- Ban all the lamers.
+4- Quitter le script --->[].${NC}
+"
+read choix
+stty echo
+if [ $choix = 1]; then
+  printf "Ecriture des regles" #ALED LE FR marion ?!
+  echo "${IP} -F INPUT
+        ${IP} -F OUTPUT
+        ${IP} -F FORWARD
+        ${IP} -x
+        
+        MARION
+
+        ${IP} -A INPUT -s 104.131.0.0/16 -j DROP
+        ${IP} -A INPUT -s 104.236.0.0/16 -j DROP
+        ${IP} -A INPUT -s 104.248.0.0/16 -j DROP
+        ${IP} -A INPUT -s 107.170.0.0/16 -j DROP
+        ${IP} -A INPUT -s 128.199.0.0/16 -j DROP
+        ${IP} -A INPUT -s 138.68.0.0/16 -j DROP
+        ${IP} -A INPUT -s 134.122.0.0/16 -j DROP
+        ${IP} -A INPUT -s 138.197.0.0/16 -j DROP
+        ${IP} -A INPUT -s 134.209.0.0/16 -j DROP" >  /etc/iptables/iptablesMarion.rules
+fi
